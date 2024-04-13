82bb482b0847892f7638cfb8d5c291dd314fe6b7
makuiken <mkuiken18@outlook.com>
2024-04-13 14:18:56 -0400

    Add user progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,16 +2,26 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { Header } from "./header";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gpa-[48px] px-6">
       <StickyWrapper>
-        <UserProgress 
+        <UserProgress
           activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
           hearts={5}
           points={100}
-          hasActiveSubscription={false} 
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
